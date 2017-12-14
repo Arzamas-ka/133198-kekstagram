6fc26c34c7df1e035fc1c9af5c5542514bca31e7
Liudmila <signum.ru@gmail.com>
2017-12-15 00:53:17 +0300

    added validation

--- js/pictures.js
+++ js/pictures.js
@@ -156,3 +156,148 @@ function getClicks() {
 }
 
 getClicks();
+
+// Показ/скрытие формы кадрирования
+var filterPopup = document.querySelector('.upload-overlay');
+var crossPopup = document.querySelector('.upload-form-cancel');
+var btn = document.querySelector('.upload-form-submit');
+
+function hideFilter() {
+  filterPopup.classList.add('hidden');
+}
+
+function filterEscHandler(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    hideFilter();
+  }
+}
+
+function filterEnterHandler(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    hideFilter();
+  }
+}
+
+function openFilter() {
+  document.querySelector('#upload-file').addEventListener('change', function () {
+    filterPopup.classList.remove('hidden');
+  });
+}
+// Добавить обработчики для альтернативного ввода с клавиатуры
+function handleUserActions() {
+  crossPopup.addEventListener('click', function () {
+    hideFilter();
+  });
+  crossPopup.addEventListener('keydown', filterEnterHandler);
+  document.addEventListener('keydown', filterEscHandler);
+  btn.addEventListener('keydown', filterEnterHandler);
+  // Если фокус находится на форме ввода комментария, то форма закрываться не должна
+  var commentInput = document.querySelector('.upload-form-description');
+
+  commentInput.addEventListener('focus', function () {
+    document.removeEventListener('keydown', filterEscHandler);
+  });
+
+  commentInput.addEventListener('blur', function () {
+    document.addEventListener('keydown', filterEscHandler);
+  });
+}
+
+openFilter();
+handleUserActions();
+
+// Работа с фильтрами
+var imgPreview = filterPopup.querySelector('.effect-image-preview');
+function changeEffect(evt) {
+  if (evt.target.tagName === 'INPUT') {
+    evt.stopPropagation();
+
+    var effectName = evt.target.id;
+    // обрезаем класс
+    effectName = effectName.split('-').slice(1).join('-');
+    // удаляем старый
+    imgPreview.className = 'effect-image-preview';
+    // вешаем новый
+    imgPreview.classList.add(effectName);
+  }
+}
+
+filterPopup.querySelector('.upload-effect-controls').addEventListener('click', changeEffect, true);
+
+// Изменение масштаба изображения
+var decrementBtn = filterPopup.querySelector('.upload-resize-controls-button-dec');
+var incrementBtn = filterPopup.querySelector('.upload-resize-controls-button-inc');
+var totalBtn = filterPopup.querySelector('.upload-resize-controls-value');
+var step = parseInt(totalBtn.step, 10);
+var minScale = totalBtn.getAttribute('minlength');
+var maxScale = totalBtn.getAttribute('maxlength');
+
+var minPercent = parseInt(minScale, 10);
+var maxPercent = parseInt(maxScale, 10);
+var value = parseInt(totalBtn, 10);
+// уменьшение изображения
+decrementBtn.addEventListener('click', function () {
+  var minStep = parseInt(totalBtn.value, 10) - step;
+  if (value < minPercent) {
+    return;
+  }
+  totalBtn.value = minStep + '%';
+  var transformScale = minStep / 100;
+  imgPreview.style.transform = 'scale(' + transformScale + ')';
+});
+
+// увеличение изображения
+incrementBtn.addEventListener('click', function () {
+  if (parseInt(totalBtn.value, 10) >= maxPercent) {
+    return;
+  }
+  var maxStep = parseInt(totalBtn.value, 10) + step;
+  totalBtn.value = maxStep + '%';
+  var transformScale = maxStep / 100;
+  imgPreview.style.transform = 'scale(' + transformScale + ')';
+});
+
+// Хэш-теги
+var hashTagInput = filterPopup.querySelector('.upload-form-hashtags');
+var submitPopup = filterPopup.querySelector('.upload-form-submit');
+var lengthTag = 20;
+var maxLenghtHashtag = 5;
+
+function validateHashTags(hashTagValue) {
+  hashTagValue = hashTagValue.toLowerCase();
+  hashTagValue = hashTagValue.trim();
+  if (hashTagValue[0] !== '#') {
+    hashTagInput.setCustomValidity('Хэш-тег начинается с символа #');
+    hashTagInput.style.outlineColor = 'red';
+    return;
+  }
+
+  hashTagValue = hashTagValue.split();
+  if (hashTagValue.length > maxLenghtHashtag) {
+    hashTagInput.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+    hashTagInput.style.outlineColor = 'red';
+    return;
+  }
+
+  for (var j = 1; j < hashTagValue.length; j++) {
+    if (hashTagValue[j] === '#') {
+      if (hashTagValue[j].indexOf('#', 1) > 0) {
+        hashTagInput.setCustomValidity('Хэш-теги разделяются пробелами');
+        hashTagInput.style.outlineColor = 'red';
+      }
+    }
+    return;
+  }
+
+  for (var i = 0; i < hashTagValue.length; i++) {
+    if (hashTagValue[i].length > lengthTag) {
+      hashTagInput.setCustomValidity('Максимальная длина одного хэш-тега 20 символов');
+      hashTagInput.style.outlineColor = 'red';
+      return;
+    }
+  }
+}
+
+submitPopup.addEventListener('click', function () {
+  validateHashTags(hashTagInput.value);
+});
